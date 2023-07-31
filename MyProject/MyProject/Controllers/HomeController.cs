b052bb1ee2d3415489bb9bc09852f9db5ef2b992
nikitabelekar <nikita.b@finiq.com>
2023-07-31 18:34:22 +0530

    ASP.NetCoreMVCCRUDApp

---
+++ MyProject/MyProject/Controllers/HomeController.cs
@@ -0,0 +1,204 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyProject.Models;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+
+namespace MyProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Existing WebAPI hosted on server
+        string Baseurl = "https://getinvoices.azurewebsites.net/";
+
+        public async Task<ActionResult> Index()
+        {
+            List<Customer> CustInfo = new List<Customer>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+
+                //Data format in which request is sent
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //sending request to find WebAPI service resource using HttpClient
+                HttpResponseMessage Res = await client.GetAsync("api/Customers");
+
+                //Check response success
+
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Store response in a variable
+                    var CustResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    //deserialize reponse rescieved from WebAPI
+                    CustInfo = JsonConvert.DeserializeObject<List<Customer>>(CustResponse);
+
+                }
+
+            }
+            //Return the customer list to view
+            return View(CustInfo);
+
+            }
+
+        public async Task<ActionResult> Details(string id)
+        {
+            //if (id == null)
+            //{
+            //    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            //}
+
+            Customer custobj = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+
+                var result = await client.GetAsync($"api/Customer/{id}");
+
+                if (result.IsSuccessStatusCode)
+                {
+                    custobj = await result.Content.ReadAsAsync<Customer>();
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Server error.");
+                }
+            }
+
+           
+            return View(custobj);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("Create")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(Customer custobj)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(Baseurl);
+
+                    var response = await client.PostAsJsonAsync("api/Customer/", custobj);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Server error.");
+                    }
+                }
+            }
+            return View(custobj);
+        }
+
+        public async Task<ActionResult> Edit(string id)
+        {
+            //if (id == null)
+            //{
+            //    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            //}
+            Customer custobj = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+
+                var result = await client.GetAsync($"api/Customer/{id}");
+
+                if (result.IsSuccessStatusCode)
+                {
+                    custobj = await result.Content.ReadAsAsync<Customer>();
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Server error.");
+                }
+            }
+           
+            return View(custobj);
+        }
+
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(Customer custobj)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(Baseurl);
+                    var response = await client.PutAsJsonAsync("api/Customer", custobj);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Server error.");
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(custobj);
+        }
+
+        public async Task<ActionResult> Delete(string id)
+        {
+           
+            Customer custobj = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+
+                var result = await client.GetAsync($"api/Customer/{id}");
+
+                if (result.IsSuccessStatusCode)
+                {
+                    custobj = await result.Content.ReadAsAsync<Customer>();
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Server error.");
+                }
+            }
+
+         
+            return View(custobj);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(string id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+
+                var response = await client.DeleteAsync($"api/Customer/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                    ModelState.AddModelError(string.Empty, "Server error try after some time.");
+            }
+            return View();
+        }
+
+    }
+
+
+}
---
+++ MyProject/MyProject/Models/Customer.cs
@@ -0,0 +1,73 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.AccessControl;
+using System.Xml.Linq;
+
+namespace MyProject.Models
+{
+    public class Customer
+    {
+        [Display(Name = "ID")]
+        public int Id { get; set; }
+
+       
+        public string salutation { get; set; }
+        public string initials { get; set; }
+
+        [Display(Name = "First Name")]
+        public string firstname { get; set; }
+
+        public string firstname_ascii { get; set; }
+
+        [Display(Name = "Gender")]
+        public string gender { get; set; }
+
+        public int firstname_country_rank { get; set; }
+        public int firstname_country_frequency { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string lastname { get; set; }
+
+        public string lastname_ascii { get; set; }
+        public string lastname_country_rank { get; set; }
+        public string lastname_country_frequency { get; set; }
+
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Please enter Email ID")]
+        [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Email is not valid.")]
+        public string email { get; set; }
+        public string password { get; set; }
+
+        [Display(Name = "Country Code")]
+        public string country_code { get; set; }
+        public string country_code_alpha { get; set; }
+
+        public string country_name { get; set; }
+        public string primary_language_code { get; set; }
+        public string primary_language { get; set; }
+
+
+        [DisplayFormat(DataFormatString = "{0:C0}")]
+        [Display(Name = "Balance")]
+        public double balance { get; set; }
+
+        [Display(Name = "Phone Number")]
+        public string phone_Number { get; set; }
+
+
+        public string currency { get; set; }
+        public string partitionKey { get; set; }
+        public int rowKey { get; set; }
+
+        public DateTime timestamp { get; set; }
+
+        // public int eTag { get; set; }
+        public string[] Include { get; }
+
+
+   
+
+
+
+    }
+}
